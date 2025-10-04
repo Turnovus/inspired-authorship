00f421866ef2a8917b082e128198531cc331e1c0
Turnovus <dinolover075@gmail.com>
2025-10-04 10:08:59 -0400

    Initial quality calculations

--- Source/InspiredAuthorship/InspiredAuthorship/Thing_UnfinishedManuscript.cs
+++ Source/InspiredAuthorship/InspiredAuthorship/Thing_UnfinishedManuscript.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Collections.Generic;
 using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace InspiredAuthorship
 {
     public class Thing_UnfinishedManuscript : ThingWithComps
     {
+        public const float MaxQualityForScrapPossible = 0.6f;
+        public const float MaxWorkTimeContribution = 0.6f;
+        public const float OffsetPerSkillLevel = 0.015f;
+        public const float MaxSkillContribution = 0.6f;
+        public const float SkillCapFromWorkFactor = 2.5f;
+        public const float MaxLuckContribution = 0.5f;
+        public const float MaxQualityOffset = MaxWorkTimeContribution + MaxSkillContribution + MaxLuckContribution;
+        
         public Pawn author;
         public int ticksWorked = 0;
 
@@ -14,6 +25,41 @@ namespace InspiredAuthorship
         {
             // TODO: Generate finished book
             Destroy();
+        }
+
+        public float GetQualityPreProcessedNow(out float luck)
+        {
+            float fromWork = GetOffsetFromWork();
+            luck = Rand.Range(0f, MaxLuckContribution);
+            return fromWork + GetOffsetFromSkills(fromWork) + luck;
+        }
+        
+        public float GetOffsetFromWork() => Mathf.Min(MaxWorkTimeContribution, ticksWorked / (10f * GenDate.TicksPerDay));
+
+        public float GetOffsetFromSkills(float workOffset)
+        {
+            List<SkillRecord> bestSkills = new List<SkillRecord>();
+            for (int i = 0; i < 3; i++)
+            {
+                SkillRecord highestSkill = null;
+                foreach (SkillRecord skill in author.skills.skills)
+                {
+                    if (!bestSkills.Contains(skill) && !skill.TotallyDisabled && (highestSkill == null || highestSkill.levelInt < skill.levelInt))
+                        highestSkill = skill;
+                }
+
+                if (highestSkill != null)
+                    bestSkills.Add(highestSkill);
+                else
+                    break;
+            }
+
+            int levels = 0;
+            foreach (SkillRecord skill in bestSkills)
+                levels += skill.levelInt;
+
+            float preWorkCap = Mathf.Min(MaxSkillContribution, OffsetPerSkillLevel * levels);
+            return Mathf.Min(preWorkCap, workOffset * SkillCapFromWorkFactor);
         }
 
         public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
@@ -34,6 +80,42 @@ namespace InspiredAuthorship
             s += "Author: {0}".Formatted(author.LabelShort);
             s += "\nTime spent writing: {0}".Formatted(ticksWorked.ToStringTicksToPeriod(false));
             return s;
+        }
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            IEnumerable<Gizmo> baseGizmos = base.GetGizmos();
+            if (!baseGizmos.EnumerableNullOrEmpty())
+                foreach (Gizmo baseGizmo in baseGizmos)
+                    yield return baseGizmo;
+
+            if (!DebugSettings.ShowDevGizmos)
+                yield break;
+
+            yield return new Command_Action()
+            {
+                defaultLabel = "DEV: +1 hour",
+                action = delegate
+                {
+                    ticksWorked += GenDate.TicksPerHour;
+                },
+            };
+
+            yield return new Command_Action()
+            {
+                defaultLabel = "DEV: Log quality",
+                action = delegate
+                {
+                    float fromWork = GetOffsetFromWork();
+                    Log.Message("Overall: {0}. From work: {1}. From skill: {2}/{3}. From luck: {4}.".Formatted(
+                        GetQualityPreProcessedNow(out float luck).ToStringDecimalIfSmall(),
+                        fromWork.ToStringDecimalIfSmall(),
+                        GetOffsetFromSkills(fromWork).ToStringDecimalIfSmall(),
+                        GetOffsetFromSkills(1000f).ToStringDecimalIfSmall(),
+                        luck.ToStringDecimalIfSmall()
+                        ));
+                },
+            };
         }
 
         public override void ExposeData()
