473de0056206c030d9b0b22d3166bacfeb24462e
Turnovus <dinolover075@gmail.com>
2025-10-17 11:57:41 -0400

    Add mod settings menu

--- Source/InspiredAuthorship/InspiredAuthorship/Mod/InspiredAuthorship_Mod.cs
+++ Source/InspiredAuthorship/InspiredAuthorship/Mod/InspiredAuthorship_Mod.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace InspiredAuthorship
 {
     public class InspiredAuthorship_Mod : Mod
     {
+        public const float HeaderHeight = 30f;
+        public const float TabsHeight = 35f;
+        
+        public SettingsTab activeTab = SettingsTab.General;
+        
         public InspiredAuthorship_Mod(ModContentPack content) : base(content) {}
 
         public InspiredAuthorship_ModSettings Settings => GetSettings<InspiredAuthorship_ModSettings>();
@@ -11,5 +18,38 @@ namespace InspiredAuthorship
         public static InspiredAuthorship_Mod LoadedMod => LoadedModManager.GetMod<InspiredAuthorship_Mod>();
 
         public BookDatabase Database => Settings.Database;
+
+        public override string SettingsCategory() => "Inspired Authorship";
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Rect drawRect = new Rect(inRect);
+            drawRect.yMin += HeaderHeight;
+            DrawTabs(drawRect);
+            drawRect.yMin += TabsHeight;
+        }
+
+        private void DrawTabs(Rect inRect)
+        {
+            List<TabRecord> tabs = new List<TabRecord>();
+            tabs.Add(GetTabRecord(SettingsTab.General));
+            tabs.Add(GetTabRecord(SettingsTab.Spawning));
+            tabs.Add(GetTabRecord(SettingsTab.Database));
+            Rect tabRect = new Rect(inRect);
+            tabRect.height = TabsHeight;
+            TabDrawer.DrawTabs(tabRect, tabs);
+        }
+
+        private TabRecord GetTabRecord(SettingsTab forTab) => new TabRecord(
+            ("InspiredAuthorship.Enums.SettingsTab." + forTab.ToString()).Translate(),
+            () => activeTab = forTab,
+            activeTab == forTab);
+
+        public enum SettingsTab
+        {
+            General,
+            Spawning,
+            Database,
+        }
     }
 }
